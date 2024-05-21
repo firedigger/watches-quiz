0b6033085a22423e1caf3ab637f87ddb65f6ae76
Aleksandr Makarov <gif.inoe@gmail.com>
2024-05-21 21:57:58 +0300

    Require node 18

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,5 @@
-import { Component, Inject, OnInit, inject } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
-import { Watches } from './watches';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
-import { FormsModule } from '@angular/forms';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -14,6 +9,5 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'watches-quiz';
-  
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,5 @@ import { LeaderboardComponent } from './leaderboard/leaderboard.component';
 import { GameComponent } from './game/game.component';
 
 export const routes: Routes = [{ path: '', redirectTo: '/game', pathMatch: 'full' },
-{ path: 'game', component: GameComponent },
-{ path: 'leaderboard', component: LeaderboardComponent }];
+{ path: 'game', component: GameComponent, title: 'Game' },
+{ path: 'leaderboard', component: LeaderboardComponent, title: 'Leaderboard' }];
